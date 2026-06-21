a7520ea6828329468952b429ae14c3eae71be16b
Naman khurana <nkhurana608@gmail.com>
2026-06-21 11:24:13 +0530

    Time: 15 ms (62.77%), Space: 93.4 MB (50.45%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,13 @@
 import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-      Arrays.sort(nums);
-      for(int i = 1 ; i < nums.length ; i++){
-        if(nums[i-1] == nums[i]){
-            return true;
+        HashSet <Integer> set = new HashSet<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
         }
-      } 
-      return false;
+        return false;
     }
 }
