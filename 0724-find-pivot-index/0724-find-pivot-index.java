65658f0e65d53552081adf520d733adc4ce00cd3
Naman khurana <nkhurana608@gmail.com>
2026-06-28 00:49:58 +0530

    Time: 1 ms (98.57%), Space: 47.5 MB (39.88%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,21 +1,19 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int rightsum=0;
-        for(int i=0; i<nums.length;i++){
+        int rightsum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
             rightsum += nums[i];
         }
-
-        int leftsum=0;
-        for(int i=0;i<nums.length;i++){
+        int leftsum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
             rightsum -= nums[i];
 
-            if(rightsum==leftsum){
+            if(rightsum == leftsum){
                 return i;
             }
 
             leftsum += nums[i];
         }
         return -1;
-    
-}
+    }
 }
