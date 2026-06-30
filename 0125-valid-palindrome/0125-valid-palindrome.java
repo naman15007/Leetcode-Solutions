b1eb9b136f5b43effbd4284b97398873ce6d3fd1
Naman khurana <nkhurana608@gmail.com>
2026-06-30 17:18:21 +0530

    Time: 15 ms (20.64%), Space: 47.3 MB (14.05%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        String r = new StringBuilder(str).reverse().toString();
+        return str.equals(r);
+    }
+}
