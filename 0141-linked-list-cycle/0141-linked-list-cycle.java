c64d9ce3a5cc027cd91a8c8de03131d0fc7d273c
Naman khurana <nkhurana608@gmail.com>
2026-07-05 11:02:49 +0530

    Time: 1 ms (13.71%), Space: 46.7 MB (71.98%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,8 +13,8 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head , fast = head;
         while(fast != null && fast.next != null){
-            fast = fast.next.next;
             slow = slow.next;
+            fast = fast.next.next;
             if(slow.equals(fast) && fast != null){
                 return true;
             }
