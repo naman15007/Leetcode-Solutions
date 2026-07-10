8a497391fb746f3d23b08a4eba3b4834abf8e27c
Naman khurana <nkhurana608@gmail.com>
2026-07-10 11:15:19 +0530

    Time: 3 ms (100%), Space: 175.7 MB (28.02%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -3,16 +3,15 @@ class Solution {
          int pos = 0;
          int neg = 1;
          int n = nums.length;
-         int [] ans = new int [n];
-
-         for(int num: nums){
+         int [] ans = new int[n];
+         for(int num : nums){
             if(num>0){
-                ans[pos]=num;
-                pos = pos + 2;
+                ans[pos] = num;
+                pos += 2;
             }
             else{
-                ans[neg]= num;
-                neg= neg+2;
+                ans[neg] = num;
+                neg += 2;
             }
          }
          return ans;
