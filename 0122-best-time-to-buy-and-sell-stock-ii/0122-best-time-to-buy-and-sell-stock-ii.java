90e7502f505a1b92322bcf303419e1dc08464509
Naman khurana <nkhurana608@gmail.com>
2026-08-28 21:20:49 +0530

    Time: 0 ms (100%), Space: 46.3 MB (83.93%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int totalprofit = 0 ;
-        for(int i = 0 ; i < prices.length-1 ; i++){
+        int maxprofit = 0;
+        for(int i = 0 ; i < prices.length - 1 ; i++){
             if(prices[i+1] > prices[i]){
-                totalprofit += prices[i+1] - prices[i];
+                maxprofit += prices[i+1] - prices[i];
             }
         }
-        return totalprofit;
+        return maxprofit;
     }
 }
