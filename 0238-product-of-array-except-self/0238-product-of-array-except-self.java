0a3499e3cb3a17bb9e3c89882c0be64bfcc05ca5
Naman khurana <nkhurana608@gmail.com>
2026-08-30 14:13:31 +0530

    Time: 2 ms (93.45%), Space: 64.9 MB (82.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int [] pfe = new int[n];
-        int [] pfs = new int[n];
-        int [] ans = new int[n];
+        int [] pfs = new int[n]; // 1 , 2 , 8, 6
+        int [] pfe = new int[n]; // 48 , 48 , 24 , 6
+        int [] ans = new int[n]; // 48 , 24 , 12 , 8
         pfs[0] = nums[0];
-        for(int i = 1; i<n; i++){
-            pfs[i] = pfs[i-1]*nums[i];
+        for(int i = 1 ; i < n ;i++){
+            pfs[i] = nums[i] * pfs[i-1];
         }
-        pfe[n-1]= nums[n-1];
-        for(int i=n-2 ; i>=0 ;i--){
-            pfe[i] = pfe[i+1]*nums[i];
+        pfe[n-1] = nums[n-1];
+        for(int i = n-2 ; i >= 0 ; i--){
+            pfe[i] = nums[i] * pfe[i+1];
         }
-        ans[0] = pfe[1];
         ans[n-1] = pfs[n-2];
-        for(int i = 1 ; i<n-1; i++ ){
-            ans[i] = pfe[i+1]*pfs[i-1];
+        ans[0] = pfe[1];
+        for(int i  = 1 ; i < n-1 ; i++){
+            ans[i] = pfs[i-1] * pfe[i+1];
         }
         return ans;
     }
-}
+}  
